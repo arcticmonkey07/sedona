9e2d35f2fa912d7d0d2cc9ac5cd1130c4795fcc6
Peter <arcticmonkey07@gmail.com>
2018-08-02 21:27:50 +0300

    Завершил верстку

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var popup = document.querySelector(".search-popup");
 var link = document.querySelector(".search-button");
 
 link.addEventListener("click", function(event) {
-    event.preventDefault(event);
-    popup.classList.toggle("search-popup-show");
+  event.preventDefault(event);
+  popup.classList.add("popup-error");
+  popup.classList.toggle("search-popup-show");
 });
